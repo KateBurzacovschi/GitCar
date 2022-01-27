7d4a5e7ef13fdd2a0c1a8cdbf17a14f935ff39df
kate <ecaterina.burzacovschi@gmail.com>
2022-01-27 12:47:17 -0500

    the red car is redy for the trial

--- src/company/Main.java
+++ src/company/Main.java
@@ -3,6 +3,10 @@ package company;
 public class Main {
     public static void main(String[] args) {
         Carbasics car=new Carbasics();
-        car.color="green";
+        car.color="red";
+        car.engine="9999k";
+        car.door="2";
+        car.number="2344444";
+        System.out.println(car.color);
     }
 }
